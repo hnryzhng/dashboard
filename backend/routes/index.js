4f6981a3ac293b29e1e6f7ade820597ae8b09d92
Henry <hnryzhng@gmail.com>
2020-01-14 12:35:21 -0600

    init commit

---
+++ backend/routes/index.js
@@ -0,0 +1,21 @@
+// routes/index.js
+
+var router = require('express').Router();
+
+
+// ROUTES
+/**
+
+// @route GET api/routeA
+// @desc Does this
+// @access Public
+
+router.post('/routeB', (req, res) => {
+	
+	const something = req.body.sName;
+
+});
+
+**/
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const cors = require("cors");
+
+require('dotenv').config()	// load env vars
+
+// IMPORT MY EXTERNAL MODULES
+// const moduleA = require(path.join(__dirname, "/moduleA.js"));
+// const pingHeroku = require(path.join(__dirname, '/ping-heroku.js'));
+// pingHeroku("https://<myherokuname>.herokuapp.com", millisecondsDelay);
+
+// INSTANTIATE APP 
+const app = express();
+const api_port = process.env.PORT || 3001;
+
+/**
+// LOAD MIDDLEWARE
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// DATABASE
+const dbRoute = process.env.MONGOLAB_URI;	// use env var to hide db key for production build deployment to Heroku
+const dbRoute = require("./config/keys.js").mongoURI;	// cloud db url stored in config file
+
+mongoose
+	.connect(
+		dbRoute,
+		{useNewUrlParser: true}
+	)
+	.then(() => console.log("connected to MongoDB database"))
+	.catch((err) => console.log("error connecting to MongoDB:", err));
+**/
+
+// SERVE REACT SCRIPTS
+
+app.use(express.static(path.join(__dirname, '../client/build')));	// Adds the react production build to serve react requests
+
+app.get('*', (req, res) => {
+	// serve react front-end for all non-specified routes
+	res.sendFile(path.join(__dirname + '../client/build/index.html'));
+});
+
+
+// ROUTES
+app.use("/api", require('./routes'));	// routes for api requests: root/api/<specific_route>
+
+module.exports = app.listen(api_port, () => console.log(`Listening to ${api_port}`) );
