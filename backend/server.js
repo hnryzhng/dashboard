90624be955ff5c255b1de7cdc2c426f36a4bd2ec
Henry <hnryzhng@gmail.com>
2020-01-14 12:45:35 -0600

    added new components

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
 const cors = require("cors");
+const path = require("path");
 
 require('dotenv').config()	// load env vars
 
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,40 +4,15 @@ import axios from 'axios';
 class App extends Component {
 
   state = {
-  	something: null,
-    error: null
   }
 
-  sendSomething = (event) => {
-    event.preventDefault();
-
-    const s = this.state.something;
-    
-    axios.post("/api/routeB", {
-            sName: s
-          })
-          .then(response => response.data)
-          .then(data => {
-            if (data.success) {
-              console.log("response data:", data);
-            } else {      
-              this.setState({ error: data.error })  // sets error message 
-            }
-
-          })
-          .catch(err => console.log("error:", err));
-
-  }
   render() {
     
     return (
       <div>
       	<p>Hello World</p>
 
-        <form onSubmit={ this.sendSomething }>
-          <input type="text" style={{ width: "300px" }} placeholder="Send Something!" name="sName" onChange={ event => this.setState({ something: event.target.value }) } />
-          <button type="submit">SUBMIT ME TO YOUR TORTURE</button >
-        </form>
+
 
       </div>
     );
