eedb97c9b9b97d1ac5734f1ecd4265e146b5f54f
Henry <hnryzhng@gmail.com>
2020-01-18 19:23:06 -0600

    work on convert datda module

--- backend/processData/convertData.js
+++ backend/processData/convertData.js
@@ -2,9 +2,39 @@
 
 // convert JSON into data format for db storage in dataSource collection given path and data type
 
+const path = require('path');
+
+var storeData = path.join(__dirname, 'storeData.js');
+
+// TASK BOOKMARK
+// front-end: users selects data columns and chart type? 
+// front-end OR backend: then validate if columns of data can be rendered from selected chart type?
+// back end: if can be rendered, then send req with body params of which columns and chart type, then create appropriate dataset and send in response?  
+// back end: add chart type and correspondingly processed dataset to generatedDatasets of the dataSource record
+
+
+checkTileTypes() {
+	// example output: ["chart", "bar", "count"]
+
+	// return tileTypesArray
+
+}
+
+
 var convertData = function(jsonLinearArray) {
-	// console.log('convertData jsonLinearArray:', jsonLinearArray);
-	// console.log('typeof jsonLinearArray:', typeof jsonLinearArray);
+
+	const record = {
+		// id
+		// timestamp
+		title: "",
+		description: "",
+		data: jsonLinearArray,
+		generatedDatasets: []	// [{ tileType STR, dataset OBJ }, ...]	
+		// TASK: limit number of datasets depending on permissions
+	}
+
+	// storeData(record);
+
 };
 
 convertData.lineChart = (jsonObj) => {
