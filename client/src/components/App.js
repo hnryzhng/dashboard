e68e49ed0be866a030a14174c81b881aa17e9ab3
Henry <hnryzhng@gmail.com>
2020-01-20 13:37:47 -0600

    read csv in App front-end

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 // import dependencies
 import axios from 'axios';
+import Papa from 'papaparse';
 
 // import styles
 import '../styles.css'
@@ -19,7 +20,11 @@ class App extends Component {
     tilesList: [],  // [{ str tile_id, num tile_index, str tile_type, str dataSource_id, array generated_dataset, (obj tileElement) }]
     dataSourcesList: [], // [{ str dataSource_id, str name }]
     selectedTileType: "",
-    selectedDataSourceID: ""
+    selectedFile: null,
+    currentDataSource: null,
+    currentDataSourceHeadings: [],
+    currentDataSourceID: "",
+    currentDataSourceName: ""
   }
 
   componentDidMount() {
@@ -34,7 +39,7 @@ class App extends Component {
 
     var tilesList = this.state.tilesList;
     var tType = this.state.selectedTileType;
-    var dID = this.state.selectedDataSourceID;
+    var dID = this.state.currentDataSourceID;
 
     // TASK: BOOKMARK
     // check to see if selected tile type can be paired with selected data source
@@ -75,6 +80,8 @@ class App extends Component {
       // generate dataset in backend for particular tile based on user selection of columns or other feature 
       // create processTileData folder?
 
+      // addTile -> addDataSource -> addTile: send data path with user selected columns (e.g., selected headings from csv array)
+
 
       // if successfully stored in db, then... 
       // add tile object to list
@@ -102,8 +109,41 @@ class App extends Component {
 
   }
 
-  addDataSource = () => {
+  updateDataStates = (results) => {
+		// console.log('papaparsed results data', results.data)
+		const parsedData = results.data;
+		console.log('papaparsed results:', results)
+
+		const selectedFile = this.state.selectedFile;
+
+		this.setState({ currentDataSource: parsedData });
+		this.setState({ currentDataSourceHeadings: parsedData[0] });
+		this.setState({ currentDataSourceName: selectedFile.name})
+
+		// TASK BOOKMARK: use content hash to create data source id
+		// hashedID = hashContent function;
+		// this.setState({ currentDataSourceID: hashedID })
+
+		console.log('state currentDataSource:', this.state.currentDataSource)
+		console.log('state currentDataSourceHeadings:', this.state.currentDataSourceHeadings);
+		console.log('state currentDataSourceName:', selectedFile.name);
+		// console.log('state currentDataSourceID:', this.state.currentDataSourceID);
+
+  }
+
+  addDataSource = (event) => {
     // add data source object upon user select
+
+    event.preventDefault();
+
+    // parse selected csv file
+    // TASK: create HTML table from parsed csv: https://www.js-tutorials.com/javascript-tutorial/reading-csv-file-using-javascript-html5/
+    // set state for selected column headings of current data source
+    console.log("uploaded file:", this.state.selectedFile);
+    Papa.parse(this.state.selectedFile, {
+    	header: false,
+		complete: this.updateDataStates
+	});
 
     // TASK: check if data source name for particular user already exists
     // TASK: limits per user?
@@ -123,7 +163,7 @@ class App extends Component {
 
     console.log("selected tile type:", this.state.selectedTileType);
 
-    console.log("selected data source:", this.state.selectedDataSourceID);
+    console.log("selected data source:", this.state.currentDataSourceID);
     
     let tilesDisplay;
     const tilesList = this.state.tilesList;
@@ -159,7 +199,7 @@ class App extends Component {
             <option value="line">Line Chart</option>
           </select>
 
-          <select id="data-source" value={ this.state.selectedDataSourceID } onChange={ (event)=>{ this.setState({ selectedDataSourceID: event.target.value }) } }>
+          <select id="data-source" value={ this.state.currentDataSourceID } onChange={ (event)=>{ this.setState({ currentDataSourceID: event.target.value }) } }>
             <option defaultValue="choose data source">generate list of dropdown based on tile type?</option>            
             <option value="mock-bar-data">Bar data</option>
             <option value="mock-line-data">Line data</option>
@@ -171,6 +211,13 @@ class App extends Component {
           <button type="button" onClick={ () => { this.addDataSource() } }>ADD DATA</button>
 
         </div>
+
+        <form className="form-inline" onSubmit={ this.addDataSource }>
+        	<div className="form-group">
+        		<input type="file" id="uploadedFile" className="form-control" placeholder="upload your data source" onChange={ event => this.setState({ selectedFile: event.target.files[0] }) } />
+        		<button type="submit" id="fileUploadSubmit" className="btn btn-primary">submit file</button>
+        	</div>
+        </form>
 
 
       </div>
